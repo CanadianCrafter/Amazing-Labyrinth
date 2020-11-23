721a86afe0494c5dac486eff6502b96579708b77
mapleparty <74315672+mapleparty@users.noreply.github.com>
2020-11-23 13:46:23 -0500

    main method added

--- src/AmazingLabyrinthTest.java
+++ src/AmazingLabyrinthTest.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 
+import javax.swing.JFrame;
+
 public class AmazingLabyrinthTest {
 	
 	public static void main(String[] args) throws IOException {
@@ -8,6 +10,11 @@ public class AmazingLabyrinthTest {
 //		System.out.print("Success");
 		int arr[]={0,1};
 		new Initialize(5, arr) ;
+		
 	}
+	
+	
+
+	
 	
 }
